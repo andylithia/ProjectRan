404e5c3d9142e3e165ef27fd32faaac9ed35f9d2
AndyLithia <andylithia@dyn-129-236-176-180.dyn.columbia.edu>
2021-12-03 16:08:11 -0500

    1

--- FPU/icverilog/run_fir_tb.sh
+++ FPU/icverilog/run_fir_tb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-iverilog -Wall -o W4823_FIR_tb.vvp ../verilog/FIR_tb.v ../verilog/FIR.v ../verilog/FPALU_dummy.v ../verilog/sp_sram.v
+iverilog -Wall -o W4823_FIR_tb.vvp ../verilog/FIR_tb.v ../verilog/FIR.v ../verilog/FPALU_dummy.v ../verilog/sp_sram.v ../verilog/FPALU.v ../verilog/icgc.v ../verilog/misc.v
 vvp W4823_FIR_tb.vvp
 gtkwave W4823_FIR_tb.vcd W4823_FIR_tb.gtkw
